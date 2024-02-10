85926dc346ee7693c5c98bab7d95e828195b631e
ThatIsDreamer <43915450+ThatIsDreamer@users.noreply.github.com>
2024-02-10 16:06:18 +0300

    first

---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+import sys
+
+import pygame
+import requests
+
+def geocode(address):
+    # Собираем запрос для геокодера.
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}" \
+                       f"&geocode={address}&format=json"
+
+    # Выполняем запрос.
+    response = requests.get(geocoder_request)
+
+    if response:
+        # Преобразуем ответ в json-объект
+        json_response = response.json()
+    else:
+        raise RuntimeError(
+            """Ошибка выполнения запроса:
+            {request}
+            Http статус: {status} ({reason})""".format(
+                request=geocoder_request, status=response.status_code, reason=response.reason))
+
+    # Получаем первый топоним из ответа геокодера.
+    # Согласно описанию ответа он находится по следующему пути:
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
+# Получаем координаты объекта по его адресу.
+def get_coordinates(address):
+    toponym = geocode(address)
+    if not toponym:
+        return None, None
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Широта, преобразованная в плавающее число:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
+
+
+# Получаем параметры объекта для рисования карты вокруг него.
+def get_ll_span(address):
+    toponym = geocode(address)
+    if not toponym:
+        return (None, None)
+
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и Широта :
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+    # Собираем координаты в параметр ll
+    ll = ",".join([toponym_longitude, toponym_lattitude])
+
+    # Рамка вокруг объекта:
+    envelope = toponym["boundedBy"]["Envelope"]
+
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
+
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(float(l) - float(r)) / 2.0
+    dy = abs(float(t) - float(b)) / 2.0
+
+    # Собираем размеры в параметр span
+    span = f"{dx},{dy}"
+
+    return ll, span
+
+
+# Находим ближайшие к заданной точке объекты заданного типа.
+def get_nearest_object(point, kind):
+    ll = "{0},{1}".format(point[0], point[1])
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}" \
+                       f"&geocode={ll}&kind={kind}&format=json"
+
+    # Выполняем запрос к геокодеру, анализируем ответ.
+    response = requests.get(geocoder_request)
+    if not response:
+        raise RuntimeError(
+            f"""Ошибка выполнения запроса:
+            {geocoder_request}
+            Http статус: {response.status_code,} ({response.reason})""")
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первый топоним из ответа геокодера.
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"]["name"] if features else None
+
+
+
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+
+
+map_params = {
+    "ll": '37.530887,55.703118',
+    "spn": "0.002,0.002",
+    "l": "map",
+    "scale": "4.0"
+}
+
+response = requests.get(map_request, map_params)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
